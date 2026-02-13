d843981072d73192b04dbb06229a35f47a618280
Zhumash <pzhumash@gmail.com>
2026-02-14 01:04:34 +0500

    fix api url

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:5002';
+// Если мы на Vercel — берем ссылку из переменных. Если дома — localhost.
+const API_BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:5002';
 
 // Create axios instance
 const api = axios.create({
